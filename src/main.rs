9fead94166f43ea8d444b7aecad5f085bc039c18
Ceye4n <129878378+Ceye4n@users.noreply.github.com>
2023-09-14 21:24:48 +0200

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     println!("Hello, world!");
     let x = 4;
@@ -7,5 +8,10 @@ fn main() {
     let tup: (i32, bool, char)  = (1, true, 's');
     println!("{}", tup.0);
     let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
-    arr[4] = 3
+    arr[4] = 3;
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("failed to read line");
+    println!("{}", input);
 }
