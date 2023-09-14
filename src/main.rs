1ae55975315094e42a2b2090b099b3c78bf446af
Ceye4n <129878378+Ceye4n@users.noreply.github.com>
2023-09-14 20:19:36 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+    let x = 4;
+    println!("x is {}", x);
+    println!("x is {}", x);
+    const SECONDS_IN_MINUTE: u32 = 60;
+    let tup: (i32, bool, char)  = (1, true, 's');
+    println!("{}", tup.0);
+    let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
+    arr[4] = 3
+}
